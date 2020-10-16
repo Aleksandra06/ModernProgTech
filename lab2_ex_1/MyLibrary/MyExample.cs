fe3c9fc669543af2d234ac06d8bfd40bc4e482bc
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-10-16 22:18:24 +0700

    lab2 start

---
+++ lab2_ex_1/MyLibrary/MyExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public static class MyExample
+    {
+        public static int MinThree(int a, int b, int c)
+        {
+            int min = a;
+            if (b < min) min = a;
+            if (c < min) min = c;
+            return c;
+        }
+
+        public static float SumChetIndexMassiv()
+    }
+}
