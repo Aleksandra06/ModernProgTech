d9a409aaea697f3c7818b9e67ddd105ec6d9e063
SlavaYES <voen1999@gmail.com>
2020-10-31 12:49:24 +0700

    Add lab_3

---
+++ lab3_c/lab_3_c_test/lab_3_c_test.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_3_c/Functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab3ctest
+{
+	TEST_CLASS(TestSort)
+	{
+	public:
+		
+		TEST_METHOD(Sort)
+		{
+			int result_x = 3, result_y = 1, result_z = 5;
+			int extented_x = 5, extented_y = 3, extented_z = 1;
+
+			Functions::sort(result_x, result_y, result_z);
+
+			Assert::IsTrue(extented_x = result_x);
+			Assert::IsTrue(extented_y = result_y);
+			Assert::IsTrue(extented_z = result_z);
+
+		}
+	};
+}
---
+++ lab3_c/lab_3_even_digist_test/lab_3_even_digist_test.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_3_c/Functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab3evendigisttest
+{
+	TEST_CLASS(lab3evendigisttest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a = 12345;
+			int extented = 24;
+			int result = Functions::evenDigits(a);
+			Assert::AreEqual(extented, result);
+		}
+	};
+}
