b79a0f39b9828ba60df11c0a3e568cc74cb4bc0e
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-11-18 01:16:08 +0700

    add lab2

---
+++ lab2/UnitTestProject/UnitTest.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using lab2;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestTComplexDouble()
+        {
+            var testClass = new TComplex(55.89, 44.44);
+
+            Assert.AreEqual(testClass.real, 55.89);
+            Assert.AreEqual(testClass.imaginary, 44.44);
+        }
+        [TestMethod]
+        public void TestTComplexString()
+        {
+            string output = "7,5 + i * 3,1";
+            var testClass = new TComplex(output);
+
+            Assert.AreEqual(testClass.real, 7.5);
+            Assert.AreEqual(testClass.imaginary, 3.1);
+        }
+        [TestMethod]
+        public void TestTComplexStringEx()
+        {
+            Action action = new Action(Action1);
+
+            Assert.ThrowsException<ExComplex>(action);
+        }
+        private void Action1()
+        {
+            string output = "7,5te +t i * 3,1";
+            new TComplex(output);
+        }
+
+        [TestMethod]
+        public void TestCopy()
+        {
+            var test1 = new TComplex(55.89, 44.44);
+            var test2 = test1.Copy();
+
+            Assert.AreEqual(test1.real, test2.real);
+            Assert.AreEqual(test1.imaginary, test2.imaginary);
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            var test1 = new TComplex(3, 4);
+            var test2 = new TComplex(4, -1);
+            var resulr = test1.Add(test2);
+
+            Assert.AreEqual(resulr.real, 7);
+            Assert.AreEqual(resulr.imaginary, 3);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            var test1 = new TComplex(3, 4);
+            var test2 = new TComplex(4, -1);
+            var resulr = test1.Mulri(test2);
+
+            Assert.AreEqual(resulr.real, 16);
+            Assert.AreEqual(resulr.imaginary, 13);
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            var test1 = new TComplex(3, 4);
+            var test2 = new TComplex(4, -1);
+            var resulr = test1.Subtract(test2);
+
+            Assert.AreEqual(resulr.real, -1);
+            Assert.AreEqual(resulr.imaginary, 5);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            var test1 = new TComplex(3, 4);
+            var test2 = new TComplex(4, -1);
+            var resulr = test1.Divide(test2);
+
+            Assert.AreEqual(resulr.real, 0.470588,5);
+            Assert.AreEqual(resulr.imaginary, 1.117647,5);
+        }
+
+        [TestMethod]
+        public void TestSquare()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.Square();
+
+            Assert.AreEqual(resulr.real, -7);
+            Assert.AreEqual(resulr.imaginary, 24);
+        }
+
+        [TestMethod]
+        public void TestReverse()
+        {
+            var test1 = new TComplex(0, -3);
+            var resulr = test1.Reverse();
+
+            Assert.AreEqual(resulr.real, 0);
+            Assert.AreEqual(resulr.imaginary, 0.333333, 5);
+        }
+
+        [TestMethod]
+        public void TestMinus()
+        {
+            var test1 = new TComplex(0, 4);
+            var resulr = test1.Minus();
+
+            Assert.AreEqual(resulr.real, 0);
+            Assert.AreEqual(resulr.imaginary, -4);
+        }
+
+        [TestMethod]
+        public void TestAbs()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.Abs();
+
+            Assert.AreEqual(resulr, 5);
+        }
+
+        [TestMethod]
+        public void TestRad()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.Rad();
+
+            Assert.AreEqual(0.927295, resulr, 5);
+        }
+
+        [TestMethod]
+        public void TestDegree()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.Degree();
+
+            Assert.AreEqual(53.1301, resulr, 4);
+        }
+
+        [TestMethod]
+        public void TestPow()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.Pow(5);
+
+            Assert.AreEqual(resulr.real, -237, 4);
+            Assert.AreEqual(resulr.imaginary, -3116, 4);
+        }
+
+        [TestMethod]
+        public void TestRoot()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.Sqrt(3,4);
+
+            Assert.AreEqual(resulr.real, 2.567133, 5);
+            Assert.AreEqual(resulr.imaginary, 2.142468, 5);
+        }
+
+        [TestMethod]
+        public void TestRavnFalse()
+        {
+            var test1 = new TComplex(3, 4);
+            var test2 = new TComplex(4, -1);
+            var resulr = test1.Ravn(test2);
+
+            Assert.IsFalse(resulr);
+        }
+        [TestMethod]
+        public void TestRavnTrue()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.Ravn(test1);
+
+            Assert.IsTrue(resulr);
+        }
+
+        [TestMethod]
+        public void TestNeRavnFalse()
+        {
+            var test1 = new TComplex(3, 4);
+            var test2 = new TComplex(4, -1);
+            var resulr = test1.NeRavn(test2);
+
+            Assert.IsTrue(resulr);
+        }
+        [TestMethod]
+        public void TestNeRavnTrue()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.NeRavn(test1);
+
+            Assert.IsFalse(resulr);
+        }
+
+        [TestMethod]
+        public void TestGetRealNumber()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.GetRealNumber();
+
+            Assert.AreEqual(resulr, 3);
+        }
+
+        [TestMethod]
+        public void TestGetImaginaryNumber()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.GetImaginaryNumber();
+
+            Assert.AreEqual(resulr, 4);
+        }
+
+        [TestMethod]
+        public void TestGetRealString()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.GetRealString();
+
+            Assert.AreEqual(resulr, "3");
+        }
+
+        [TestMethod]
+        public void TestGetImaginaryString()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.GetImaginaryString();
+
+            Assert.AreEqual(resulr, "4");
+        }
+
+        [TestMethod]
+        public void TestGetString()
+        {
+            var test1 = new TComplex(3, 4);
+            var resulr = test1.GetString();
+
+            Assert.AreEqual(resulr, "3 + i * 4");
+        }
+    }
+}
---
+++ lab2/lab2/TComplex.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class TComplex
+    {
+        public double real { get; set; }
+        public double imaginary { get; set; }
+
+        public TComplex(double a, double b)
+        {
+            real = a;
+            imaginary = b;
+        }
+
+        public TComplex(string str)
+        {
+            try
+            {
+                str = str.Replace(" ", "");
+                var indexPlus = str.IndexOf("+");
+                var astr = str.Substring(0, indexPlus);
+                var bstr = str.Substring(indexPlus + 3, str.Length - indexPlus - 3);
+                real = Double.Parse(astr);
+                imaginary = Double.Parse(bstr);
+            }
+            catch
+            {
+                throw new ExComplex("Не получилось обработать строку");
+            }
+        }
+
+        public TComplex Copy()
+        {
+            return (TComplex) this.MemberwiseClone();
+        }
+
+        public TComplex Add(TComplex add)
+        {
+            return new TComplex(this.real + add.real, this.imaginary + add.imaginary);
+        }
+
+        public TComplex Mulri(TComplex b)
+        {
+            return new TComplex(this.real * b.real - this.imaginary * b.imaginary,
+                this.real * b.imaginary + this.imaginary * b.real);
+        }
+
+        public TComplex Square()
+        {
+            return new TComplex(this.real * this.real - this.imaginary * this.imaginary,
+                this.real * this.imaginary + this.real * this.imaginary);
+        }
+
+        public TComplex Reverse()
+        {
+            return new TComplex(this.real / (this.real * this.real + this.imaginary * this.imaginary),
+                -this.imaginary / (this.real * this.real + this.imaginary * this.imaginary));
+        }
+
+        public TComplex Subtract(TComplex b)
+        {
+            return new TComplex(this.real - b.real, this.imaginary - b.imaginary);
+        }
+
+        public TComplex Divide(TComplex anClass)
+        {
+            return new TComplex((this.real * anClass.real + this.imaginary * anClass.imaginary) / (anClass.real * anClass.real + anClass.imaginary * anClass.imaginary),
+                (anClass.real * this.imaginary - this.real * anClass.imaginary) / (anClass.real * anClass.real + anClass.imaginary * anClass.imaginary));
+        }
+
+        public TComplex Minus()
+        {
+            return new TComplex(this.real != 0 ? -this.real : 0, this.imaginary != 0 ? -this.imaginary : 0);
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(this.real * this.real + this.imaginary * this.imaginary);
+        }
+
+        public double Rad()
+        {
+            if (real > 0)
+
+                return Math.Atan(imaginary / real);
+
+            if (real == 0 && imaginary > 0)
+
+                return (Math.PI / 2);
+
+            if (real < 0)
+
+                return (Math.Atan(imaginary / real) + Math.PI);
+
+            if (real == 0 && imaginary < 0)
+
+                return (-Math.PI / 2);
+
+            return 0;
+        }
+
+        public double Degree()
+        {
+            return Rad() * 180 / Math.PI;
+        }
+
+        public TComplex Pow(int n)
+        {
+            return new TComplex(Math.Pow(Abs(), n) * Math.Cos(n * Rad()), Math.Pow(Abs(), n) * Math.Sin(n * Rad()));
+        }
+
+        public TComplex Sqrt(int powN, int rootI)
+        {
+            if (powN == 0)
+
+                return new TComplex(1, 0);
+
+            if (rootI == 0)
+                new ExComplex("Деление на 0. ");
+
+            TComplex new1 = Pow(powN);
+
+            return new TComplex(Math.Pow(new1.Abs(), 1 / (double)rootI) * Math.Cos((new1.Rad() + 2 * Math.PI * rootI) / rootI), Math.Pow(new1.Abs(), 1 / (double)rootI) * Math.Sin((new1.Rad() + 2 * Math.PI * rootI) / rootI));
+        }
+
+        public bool Ravn(TComplex anClass)
+        {
+            return (this.real == anClass.real && this.imaginary == anClass.imaginary);
+        }
+
+        public bool NeRavn(TComplex anClass)
+        {
+            return (this.real != anClass.real || this.imaginary != anClass.imaginary);
+        }
+
+        public double GetRealNumber()
+        {
+            return this.real;
+        }
+
+        public double GetImaginaryNumber()
+        {
+            return this.imaginary;
+        }
+
+        public string GetRealString()
+        {
+            return this.real.ToString();
+        }
+
+        public string GetImaginaryString()
+        {
+            return this.imaginary.ToString();
+        }
+
+        public string GetString()
+        {
+            return this.real.ToString("##,###") + ' ' + (this.imaginary >= 0 ? '+' : '-') + " i * " + this.imaginary.ToString("##,###");
+        }
+    }
+}
