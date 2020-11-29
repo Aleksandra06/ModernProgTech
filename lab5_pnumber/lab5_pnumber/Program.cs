b7bd261fc0b1f27518e9492436dcc84825a66f1f
SlavaYES <voen1999@gmail.com>
2020-11-30 02:04:51 +0700

    add lab_3(lab_5_pnumber)

---
+++ lab5_pnumber/lab5_pnumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using lab5_pnumber.pnumber;
+
+namespace lab5_pnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "1283,22";
+            string b = "15";
+            string c = "2";
+
+            string extend = "0,0";
+            IPNumber iP = new IPNumber(a, b, c);
+
+            string result = iP.Sqr().GetPString();
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ lab5_pnumber/pnumber/MyExeption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pnumber
+{
+    public class MyExeption : Exception
+    {
+        MyExeption(string Message) : base(Message)
+        {
+        }
+    }
+}
